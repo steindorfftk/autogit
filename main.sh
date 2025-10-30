ced78b569f3474d0e520af5e1a1aa396498774f3
Thiago Steindorff <steindorff.thiago@gmail.com>
2025-10-30 16:32:20 -0300

    added menu

--- main.sh
+++ main.sh
@@ -39,6 +39,95 @@ echo "Selected branch: $selected \n"
 git merge "$selected"
 }
 
+function delete_branch (){
+
+selected=$(git branch | fzf +m \
+	--height 40% \
+ 	--layout reverse \
+ 	--border \
+ 	--preview 'git -c color.ui=always diff $(git branch | grep "^*" | tr -d "* " ) $(echo {} | tr -d "* ")' \
+ 	--color bg:#222222,preview-bg:#333333
+	)
+	
+exit_exception	
+selected=$(echo $selected | tr -d "* ")
+echo "Selected branch: $selected \n"
+git branch -d "$selected"
+}
+
+function main (){
+	options=("1 - Switch branch" "2 - Merge branches" "3 - Delete branch" "4 - Exit")
+	selected=$( for opt in "${options[@]}" ; do echo $opt ; done | fzf +m \
+	--height 40% \
+ 	--layout reverse \
+ 	--border \
+ 	--color bg:#222222,preview-bg:#333333
+	
+	
+	
+	exit_exception
+	)
+	
+	case "$selected" in
+		${options[0]})
+			switch_branch 
+			exit 0;;
+		${options[1]})
+			merge_branches 
+			exit 0;;
+		${options[2]})
+			delete_branch 
+			exit 0;;
+		${options[3]})
+			switch_branch
+			exit 0;;
+		*)
+		exit 0
+	esac	
+		
+	
+
+
+
+}
+
+main
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 merge_branches
 
 
