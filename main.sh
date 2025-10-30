33fbd448e90d5601c57bb5d97a381da2a0d260bd
Thiago Steindorff <steindorff.thiago@gmail.com>
2025-10-30 16:04:36 -0300

    turned into a function

--- main.sh
+++ main.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 
-selected=$(git branch | fzf \
+function switch_branch (){
+
+selected=$(git branch | fzf +m \
 	--height 40% \
  	--layout reverse \
  	--border \
  	--preview 'git log --oneline $(echo {} | tr -d "* ")' \
  	--color bg:#222222,preview-bg:#333333
 	)
+	
+echo "> $?"	
+	
+if [ $? -eq 130 ]; then
+	echo "Exiting..."
+	exit 1
+fi	
+	
 selected=$(echo $selected | tr -d "* ")
 echo "Selected branch: $selected \n"
 git switch "$selected"
+}
+
+switch_branch
+
+
+
