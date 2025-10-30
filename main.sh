66fa732fabc461b772f71a60a8d00e5fd2714a3f
Thiago Steindorff <steindorff.thiago@gmail.com>
2025-10-30 15:48:02 -0300

    configured colors

--- main.sh
+++ main.sh
@@ -4,7 +4,8 @@ selected=$(git branch | fzf \
 	--height 40% \
  	--layout reverse \
  	--border \
- 	--preview 'git log --oneline'
+ 	--preview 'git log --oneline $(echo {} | tr -d "* ")' \
+ 	--color bg:#222222,preview-bg:#333333
 	)
 selected=$(echo $selected | tr -d "* ")
 echo "Selected branch: $selected \n"
